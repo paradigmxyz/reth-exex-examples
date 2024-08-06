3a32cb98446b2f5197e772587cd48e66f136fb97
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-08-06 21:23:58 +0200

    chore: realign with upstream

--- backfill/src/main.rs
+++ backfill/src/main.rs
@@ -6,7 +6,11 @@ use clap::{Args, Parser};
 use eyre::OptionExt;
 use futures::{FutureExt, TryStreamExt};
 use jsonrpsee::tracing::instrument;
-use reth::primitives::BlockNumber;
+use reth::{
+    primitives::{BlockId, BlockNumber, BlockNumberOrTag},
+    providers::{BlockIdReader, BlockReader, HeaderProvider, StateProviderFactory},
+};
+use reth_evm::execute::BlockExecutorProvider;
 use reth_execution_types::Chain;
 use reth_exex::{BackfillJob, BackfillJobFactory, ExExContext, ExExEvent, ExExNotification};
 use reth_node_api::FullNodeComponents;
@@ -185,13 +189,7 @@ impl<Node: FullNodeComponents> BackfillExEx<Node> {
         backfill_tx: mpsc::UnboundedSender<BackfillMessage>,
         cancel_rx: oneshot::Receiver<oneshot::Sender<()>>,
     ) {
-        let backfill = job
-            // Convert the backfill job into a parallel stream
-            .into_stream()
-            // Convert the block execution error into `eyre` error type
-            .map_err(Into::into)
-            // Process each block, returning early if an error occurs
-            .try_for_each(|chain| async move { Self::process_committed_chain(&chain).await });
+        let backfill = backfill_with_job(job);
 
         tokio::select! {
             result = backfill => {
@@ -226,19 +224,7 @@ async fn backfill_with_job<
         // Covert the block execution error into `eyre`
         .map_err(Into::into)
         // Process each block, returning early if an error occurs
-        .try_for_each(|(block, output)| async {
-            let sealed_block = block.seal_slow();
-            let execution_outcome = ExecutionOutcome::new(
-                output.state,
-                output.receipts.into(),
-                sealed_block.number,
-                vec![Requests(output.requests)],
-            );
-            let chain = Chain::new([sealed_block], execution_outcome, None);
-
-            // Process the committed blocks
-            process_committed_chain(&chain)
-        })
+        .try_for_each(|chain| async move { process_committed_chain(&chain) })
         .await
 }
 
