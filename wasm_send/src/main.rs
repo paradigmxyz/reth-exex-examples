b567b7a138101050c520f212f2b6ddacbc8a0416
Ammar Arif <evergreenkary@gmail.com>
2024-08-15 11:10:13 -0700

    cli send subsommands

--- wasm_send/src/main.rs
+++ wasm_send/src/main.rs
@@ -7,37 +7,69 @@ use std::io::Read;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    // Get the JSON-RPC URL endpoint and WASM file path from command line arguments
     let args: Vec<String> = std::env::args().collect();
-    ensure!(args.len() >= 3, "<JSON-RPC URL> <WASM file path>");
 
-    let url = &args[1];
-    let wasm_file_path = &args[2];
+    let subcommand = &args[1];
+    let url = &args[2];
 
-    // Read the WASM binary from the file specified in command line arguments
+    match subcommand.as_str() {
+        "install" => {
+            ensure!(args.len() >= 3, "Usage: install <JSON-RPC URL> <name> <WASM file path>");
+            let wasm_file_path = &args[3];
+            let name = &args[4];
+            install(url, wasm_file_path, name).await?
+        }
+
+        "start" => {
+            ensure!(args.len() >= 3, "Usage: install <JSON-RPC URL> <name>");
+            let name = &args[3];
+            start(url, name).await?
+        }
+        _ => println!("Invalid subcommand. Use 'install' or 'start'."),
+    }
+
+    Ok(())
+}
+
+async fn install(url: &str, name: &str, wasm_file_path: &str) -> eyre::Result<()> {
     let mut file = File::open(wasm_file_path)?;
     let mut wasm_bytes = Vec::new();
     file.read_to_end(&mut wasm_bytes)?;
 
-    // Serialize the WASM bytes into a base64 string
     let wasm_base64 = BASE64_STANDARD.encode(&wasm_bytes);
 
-    // Create the JSON-RPC payload
     let payload = json!({
         "jsonrpc": "2.0",
         "method": "exex_install",
         "params": {
-            "name": "my_wasm_module",
+            "name": name,
             "wasm_base64": wasm_base64
         },
         "id": 1
     });
 
-    // Send the payload to the JSON-RPC endpoint provided as an argument
     let client = Client::new();
     let res = client.post(url).json(&payload).send().await?;
 
-    println!("Response: {:?}", res.text().await?);
+    println!("Install Response: {:?}", res.text().await?);
+
+    Ok(())
+}
+
+async fn start(url: &str, name: &str) -> eyre::Result<()> {
+    let payload = json!({
+        "jsonrpc": "2.0",
+        "method": "exex_start",
+        "params": {
+            "name": name
+        },
+        "id": 1
+    });
+
+    let client = Client::new();
+    let res = client.post(url).json(&payload).send().await?;
+
+    println!("Start Response: {:?}", res.text().await?);
 
     Ok(())
 }
